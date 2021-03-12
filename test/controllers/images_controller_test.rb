1a7c737d97161dca7521d2863e050bb0eb537813
Tyler Webner <tyler.webner@appfolio.com>
2021-03-12 14:17:53 -0800

    Rubocop green

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -12,7 +12,7 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
 
   def test_show_success
     url = 'https://i.natgeofe.com/k/66d3a80c-f4c3-4410-845c-3543375eaa85/cheetah-watching.jpg'
-    tag_list = "tag1, tag2"
+    tag_list = 'tag1, tag2'
     image = Image.new(url: url, tag_list: tag_list)
     assert image.save
 
@@ -20,12 +20,12 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :success
     assert_select "img:match('src', ?)", url
-    assert_select "p", tag_list
+    assert_select 'p', tag_list
   end
 
   def test_create_valid
     url = 'https://i.natgeofe.com/k/66d3a80c-f4c3-4410-845c-3543375eaa85/cheetah-watching.jpg'
-    tag_list = "tag1, tag2, tag3"
+    tag_list = 'tag1, tag2, tag3'
     assert_difference('Image.count', 1) do
       post images_url, params: { image: { url: url, tag_list: tag_list } }
     end
@@ -46,9 +46,11 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   def test_index_success
-    Image.create(url: 'https://i.natgeofe.com/k/66d3a80c-f4c3-4410-845c-3543375eaa85/cheetah-watching.jpg', tag_list: "cheetah")
-    Image.create(url: 'https://images.theconversation.com/files/285143/original/file-20190722-11355-1peled7.jpg', tag_list: "zebra, tag2")
-    Image.create(url: 'https://www.apa.org/images/2020-03-feature-giraffe_tcm7-269465.png', tag_list: "")
+    Image.create(url: 'https://i.natgeofe.com/k/66d3a80c-f4c3-4410-845c-3543375eaa85/cheetah-watching.jpg',
+                 tag_list: 'cheetah')
+    Image.create(url: 'https://images.theconversation.com/files/285143/original/file-20190722-11355-1peled7.jpg',
+                 tag_list: 'zebra, tag2')
+    Image.create(url: 'https://www.apa.org/images/2020-03-feature-giraffe_tcm7-269465.png', tag_list: '')
 
     get images_url
 
--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -22,7 +22,8 @@ class ImageTest < ActiveSupport::TestCase
   end
 
   def test_valid_url_with_tags
-    image = Image.new(url: 'https://i.natgeofe.com/k/66d3a80c-f4c3-4410-845c-3543375eaa85/cheetah-watching.jpg', tag_list: [ "tag1", "tag2", "tag3" ] )
+    image = Image.new(url: 'https://i.natgeofe.com/k/66d3a80c-f4c3-4410-845c-3543375eaa85/cheetah-watching.jpg',
+                      tag_list: %w[tag1 tag2 tag3])
     assert image.save
   end
 end
